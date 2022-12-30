b01e5feab0f8fece986772c20941de161391e0a9
master <masterluo@dofun.cn>
2022-12-30 15:26:23 +0800

    update

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ ZSH_CONFIG_FILE=$CURRENT_DIR/.zshrc
 install()
 {
   if ! which "$1" > /dev/null 2>&1; then
-    echo "install $1 ..."
+    echo "installing $1 ..."
 
     if which apt > /dev/null 2>&1; then
       sudo apt install "$1" -y
@@ -26,12 +26,12 @@ install_zsh_plugin()
   if ( ! which "$1" > /dev/null 2>&1 ) && ( ! which "$2" > /dev/null 2>&1 ); then
       PLUGIN_PATH=$ZSH_PLUGINS/$1
       if [ -d "$PLUGIN_PATH" ]; then
-        echo "$1 existed, pull lasted version..."
+        echo "$1 existed, pull lasted version ..."
         cd "$PLUGIN_PATH" || exit 1
         git pull
         cd "$PWD" || exit 1
       else
-        echo "install $1..."
+        echo "installing $1 ..."
         git clone "$2" "$PLUGIN_PATH" || {
           echo "Error: git clone $1 failed"
           exit 1
@@ -46,40 +46,40 @@ install_zsh_plugin()
 if [ ! "$(command -v curl)" ]; then
   install curl
 else
-  echo "curl is installed..." >&2
+  echo "curl is installed ..." >&2
 fi
 
 # install wget
 if [ ! "$(command -v wget)" ]; then
   install wget
 else
-  echo "wget is installed..." >&2
+  echo "wget is installed ..." >&2
 fi
 
 # install git
 if [ ! "$(command -v git)" ]; then
   install git
 else
-  echo "git is installed..." >&2
+  echo "git is installed ..." >&2
 fi
 
 # install git flow
 if [ ! "$(command -v git-flow)" ]; then
   install git-flow
 else
-  echo "git-flow is installed..." >&2
+  echo "git-flow is installed ..." >&2
 fi
 
 # install zsh
 if [ ! "$(command -v zsh)" ]; then
   install zsh
 else
-  echo "zsh is installed..."
+  echo "zsh is installed ..."
 fi
 
 # install oh-my-zsh
 if [ ! -d $ZSH ]; then
-  echo "install oh-my-zsh..."
+  echo "installing oh-my-zsh ..."
   git clone https://github.com/robbyrussell/oh-my-zsh.git "$ZSH" || {
     printf "Error: git clone oh-my-zsh failed"
     exit 1
@@ -91,11 +91,13 @@ else
 fi
 
 # config .zshrc
-echo "make zsh config, .zshrc"
+echo "making zsh config > .zshrc"
+echo "$ZSH_CONFIG_FILE"
 if [ -d "$ZSH_CONFIG_FILE" ]; then
   echo "moving $ZSH_CONFIG_FILE to $HOME/.zshrc"
   mv "$ZSH_CONFIG_FILE" "$HOME/.zshrc"
 else
+  echo "coping zshrc-template > .zshrc"
   cp "$ZSH/templates/zshrc.zsh-template" "$HOME/.zshrc"
 fi
 
